e1f0d823fcf1114d204306e5fb6878b85628ece5
Alessandro Crimi <alessandro.crimi@iit.it>
2018-08-20 01:56:15 +0200

    Update cycling.ino

--- cycling.ino
+++ cycling.ino
@@ -1,16 +1,34 @@
 // include the library code:
 #include <LiquidCrystal.h>
+#include <Keypad.h>
+
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 8, en = 13, d4 = 12, d5 = 11, d6 = 10, d7 = 9;
+const int rs = A0, en = 13, d4 = 12, d5 = 11, d6 = 10, d7 = 9;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {8, 7, 6, 5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {4, 3, 2}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("Cycle");
+ 
 }
 
 void loop() {
@@ -19,4 +37,9 @@ void loop() {
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
+    char key = keypad.getKey();
+  
+  if (key){ 
+     lcd.print(key);
+  }
 }
